9a2e45de078a106505c6fe7411533a1ecd6118db
vijayskk <vijayskk007@gmail.com>
2022-06-24 20:03:04 +0530

    Header responsive ok

--- components/Header.js
+++ components/Header.js
@@ -4,20 +4,20 @@ import React from 'react'
 
 function Header() {
     return (
-        <div className='flex sticky shadow-lg h-20 items-center '>
+        <div className='flex sticky shadow-md h-16 md:h-20 items-center '>
             <IconButton className='ml-4' color="default">
                 <MenuSharp />
             </IconButton>
-            <img className='w-10 ml-5' src="https://cdn-icons-png.flaticon.com/512/5968/5968517.png" alt="" />
-            <h1 className='ml-2 text-gray-500 text-xl '>Docs</h1>
-            <div className='flex-grow flex items-center rounded-lg h-10 bg-gray-200 ml-20'>
+            <img className='w-10 ml-2 md:ml-5' src="https://cdn-icons-png.flaticon.com/512/5968/5968517.png" alt="" />
+            <h1 className='hidden md:inline ml-2 text-gray-500 text-xl '>Docs</h1>
+            <div className='flex-grow flex items-center rounded-lg h-10 bg-gray-200 ml-4 md:ml-20'>
                 <Search className='ml-4' />
-                <input type="text" className='w-full bg-transparent ml-2 focus:outline-none' name="" placeholder='Search docs' value="" />
+                <input type="text" className='w-full bg-transparent ml-2 focus:outline-none' placeholder='Search'/>
             </div>
-            <IconButton className='ml-8' color="default">
+            <IconButton className='ml-2 md:ml-8' color="default">
                 <Apps />
             </IconButton>
-            <Avatar className='ml-8 mr-4' src='https://avatars.githubusercontent.com/u/83910193?v=4' />
+            <Avatar className='ml-2 md:ml-8 mr-4' src='https://avatars.githubusercontent.com/u/83910193?v=4' />
         </div>
     )
 }
