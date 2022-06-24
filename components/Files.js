295d3b7ea3f7682d8b6d780104f5bc8c16bca92c
vijayskk <vijayskk007@gmail.com>
2022-06-24 21:23:19 +0530

    Home ui responsiveness ok

--- components/Files.js
+++ components/Files.js
@@ -3,52 +3,37 @@ import { IconButton } from '@mui/material'
 import React from 'react'
 
 function Files() {
+    const files = [0,1,2,3,4,5]
   return (
     <div>
       <div className='w-full flex justify-center '>
         <div className='max-w-3xl w-full '>
             <div className='sticky w-full h-20 border-b border-gray-300 flex items-center'>
-                <p className='ml-4 text-gray-600'>My Document</p>
-                <p className=' text-gray-600 ml-auto mr-4'>Date created</p>
-                <IconButton className='' color="default">
+                <p className='ml-4 text-gray-600'>My Documents</p>
+                <p className=' text-gray-500 ml-auto mr-4'>Date created</p>
+                <IconButton className='mr-4' color="default">
                     <Folder />
                 </IconButton>
             </div>
 
-            <div className='w-full h-20 border-b border-gray-200 flex items-center'>
-                <IconButton className='' color="primary">
-                    <Folder />
-                </IconButton>
-                <p className='ml-2 text-gray-600'>Document 1.docx</p>
-                <p className=' text-gray-600 ml-auto mr-4 text-sm'>14/01/04</p>
-                <IconButton className='' color="default">
-                    <MoreVert />
-                </IconButton>
-            </div>
+            {
+                files.map(()=>{
+                    return(
+                        <div className='w-full h-20 border-b border-gray-200 flex items-center'>
+                            <IconButton className='ml-4' color="primary">
+                                <Folder />
+                            </IconButton>
+                            <p className='ml-2 text-gray-600'>Document 1.docx</p>
+                            <p className=' text-gray-600 ml-auto mr-4 text-sm'>14/01/04</p>
+                            <IconButton className='' color="default">
+                                <MoreVert />
+                            </IconButton>
+                        </div>
+                    )
+                })
+            }
 
 
-            <div className='w-full h-20 border-b border-gray-200 flex items-center'>
-                <IconButton className='' color="primary">
-                    <Folder />
-                </IconButton>
-                <p className='ml-2 text-gray-600'>Document 2.docx</p>
-                <p className=' text-gray-600 ml-auto mr-4 text-sm'>14/01/04</p>
-                <IconButton className='' color="default">
-                    <MoreVert />
-                </IconButton>
-            </div>
-
-
-            <div className='w-full h-20 border-b border-gray-200 flex items-center'>
-                <IconButton className='' color="primary">
-                    <Folder />
-                </IconButton>
-                <p className='ml-2 text-gray-600'>Document 3.docx</p>
-                <p className=' text-gray-600 ml-auto mr-4 text-sm'>14/01/04</p>
-                <IconButton className='' color="default">
-                    <MoreVert />
-                </IconButton>
-            </div>
             
             
         </div>
--- components/Start.js
+++ components/Start.js
@@ -3,31 +3,29 @@ import { IconButton } from '@mui/material'
 import React from 'react'
 
 function Start() {
-  return (
-    <div className='w-full flex justify-center bg-gray-50'>
-        <div className='max-w-3xl w-full '>
-            <div className='sticky w-full h-20  flex items-center'>
-                <p className='ml-4 text-gray-600'>Start a new document</p>
-                <IconButton className='ml-auto' color="default">
-                    <MoreVert />
-                </IconButton>
-            </div>
+    return (
+        <div className='w-full flex justify-center bg-gray-50'>
+            <div className='max-w-3xl w-full '>
+                <div className='sticky w-full h-20  flex items-center'>
+                    <p className='ml-4 text-gray-600'>Start a new document</p>
+                    <IconButton className='ml-auto' color="default">
+                        <MoreVert />
+                    </IconButton>
+                </div>
 
-            <div className='w-full gap-4 grid grid-flow-row  xl:grid-cols-3 md:grid-cols-2 grid-cols-1 py-8'>
-                <IconButton className='ml-auto'>
-                    <div className='bg-white flex-col w-48 h-64 rounded-xl border border-gray-300 flex items-center justify-center mx-auto'>
+                <div className='w-full gap-4 grid grid-flow-row  xl:grid-cols-3 md:grid-cols-2 grid-cols-1 py-8'>
+                    <IconButton className='bg-white flex-col w-48 h-64 rounded-xl border border-gray-300 flex items-center justify-center mx-auto'>
                         <Add className='text-gray-600 h-20 w-20' color='primary' />
                         <p className='mt-4 text-md font-thin'>Blank</p>
-                    </div>
-                </IconButton>
-                
+                    </IconButton>
 
-                
+
+
+                </div>
+
             </div>
-            
         </div>
-    </div>
-  )
+    )
 }
 
 export default Start
