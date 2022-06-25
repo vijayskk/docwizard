e8ea07d4be65dea40428bb9ad45731e5a577ccbf
vijayskk <vijayskk007@gmail.com>
2022-06-25 08:49:22 +0530

    Nextauth implemented

--- firebase.js
+++ firebase.js
@@ -1,7 +1,5 @@
-import firebase from 'firebase/compat/app'
-import "firebase/compat/auth";
-import 'firebase/compat/firestore';
-import 'firebase/compat/storage';
+import firebase from 'firebase'
+
 
 
 
